452f24657c795865eecabd9b3622059e0a951863
MDegg <matt.degg@outlook.com>
2021-08-07 13:18:03 +0000

    Retreive latest numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,7 +15,9 @@ SHEET = GSPREAD_CLIENT.open('Love_Sandwich_Walkthrough')
 
 def get_sales_data():
     """
-    Get sales figures input from user
+    Get sales figures input from user.
+    Run a awhile loop to collect a string of data from the user
+    via a terminal, which must be a string of 6 numbers seperated by commas.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -29,7 +32,7 @@ def get_sales_data():
             print("data is valid!")
             break
 
-return sales_data
+    return sales_data
 
 def validate_data(values):
     """
@@ -50,4 +53,35 @@ def validate_data(values):
 
     return True
 
-data = get_sales_data()  
+
+def update_sales_worksheet(data):
+    """
+    update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet.....\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully. \n")
+
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    """
+
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()  
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation!\n")
+main()
